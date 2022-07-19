b81af7bc38a223f21c3dfa9d97c081451082deae
xinxu <xin872906473@163.com>
2022-07-19 20:26:24 +0800

    diff 对于中间长短相同的内容的patch；
    1.要建立新的数组的key与index的map
    2. 要建立新索引对久索引的关系newChildIndexToOldChildIndexMap

--- example/updateChildren/array2array.js
+++ example/updateChildren/array2array.js
@@ -5,13 +5,13 @@ import { h, ref } from "../../lib/ziyu-mini-vue.esm.js";
 // (AB)CD
 // (AB)
 // true -> 添加 false -> 删除
-const newArr = [
-  h("div", { key: "A" }, "A"),
-  h("div", { key: "B" }, "B"),
-  h("div", { key: "C" }, "C"),
-  h("div", { key: "D" }, "D"),
-];
-const oldArr = [h("div", { key: "A" }, "A"), h("div", { key: "B" }, "B")];
+// const newArr = [
+//   h("div", { key: "A", id: "AAA" }, "A"),
+//   h("div", { key: "B" }, "B"),
+//   h("div", { key: "C" }, "C"),
+//   h("div", { key: "D" }, "D"),
+// ];
+// const oldArr = [h("div", { key: "A" }, "A"), h("div", { key: "B" }, "B")];
 
 // 从右往左 头部不同
 // CD(AB)
@@ -24,6 +24,30 @@ const oldArr = [h("div", { key: "A" }, "A"), h("div", { key: "B" }, "B")];
 //   h("div", { key: "B" }, "B"),
 // ];
 // const oldArr = [h("div", { key: "A" }, "A"), h("div", { key: "B" }, "B")];
+
+// 过滤完左右对比中间
+// (AB)EFG(CD)
+// (AB)FEXN(CD)
+const newArr = [
+  h("div", { key: "A" }, "A"),
+  h("div", { key: "B" }, "B"),
+  h("div", { key: "E", id: "next E" }, "E"),
+  h("div", { key: "F", id: "next F" }, "F"),
+  h("div", { key: "G" }, "G"),
+  h("div", { key: "C" }, "C"),
+  h("div", { key: "D" }, "D"),
+];
+const oldArr = [
+  h("div", { key: "A" }, "A"),
+  h("div", { key: "B" }, "B"),
+  h("div", { key: "F", id: "pre F" }, "F"),
+  h("div", { key: "E", id: "pre E" }, "E"),
+  h("div", { key: "X" }, "X"),
+
+  h("div", { key: "N" }, "N"),
+  h("div", { key: "C" }, "C"),
+  h("div", { key: "D" }, "D"),
+];
 
 export const array2array = {
   setup() {
