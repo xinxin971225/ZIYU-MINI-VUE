17ee0574e64ceaec708ce5d609ede49f3ebf300d
xinxu <xin872906473@163.com>
2022-07-21 15:25:23 +0800

    first parse parseInterpolation

---
+++ src/complier-core/tests/parse.spec.ts
@@ -0,0 +1,15 @@
+import { NodeTypes } from "../src/ast";
+import { baseParse } from "../src/parse";
+
+describe("Parse", () => {
+  it(" simple interpolation", () => {
+    const ast = baseParse("{{message}}");
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.INTERPOLATION,
+      content: {
+        type: NodeTypes.SIMPLE_EXPRESSION,
+        content: "message",
+      },
+    });
+  });
+});
