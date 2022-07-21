ba5a71ca4a5270a55385220c7e4ae44aa393f1cc
xinxu <xin872906473@163.com>
2022-07-21 16:43:41 +0800

    parse tag

--- src/complier-core/src/parse.ts
+++ src/complier-core/src/parse.ts
@@ -7,21 +7,43 @@ export function baseParse(content: string) {
 
 function parseChildren(context) {
   const nodes: any[] = [];
-  const node = parseInterpolation(context);
-  nodes.push(node);
+  const s = context.source;
+  if (s.startsWith("{{")) {
+    const node = parseInterpolation(context);
+    nodes.push(node);
+  } else if (/^<[a-z]/i.test(s)) {
+    const node = parseElement(context);
+    nodes.push(node);
+  }
 
   return nodes;
 }
+function parseElement(context) {
+  const element = parseTag(context);
+  parseTag(context);
 
+  return element;
+}
+function parseTag(context) {
+  // 解析出div
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  // 删掉解析过的东西
+  advanceBy(context, match[0].length + 1);
+  return {
+    type: NodeTypes.ELEMENT,
+    tag: tag,
+  };
+}
 function parseInterpolation(context) {
   // source -> "{{message}}"
   const startDelimiter = "{{";
   const closeDelimiter = "}}";
   const { source } = context;
-  const closeIndex = source.indexOf("}}", startDelimiter.length);
-  const content = source.slice(startDelimiter.length, closeIndex);
-  context.source = source.slice(closeIndex + closeDelimiter.length);
-  console.log(content, context.source);
+  const closeIndex = source.indexOf(closeDelimiter, startDelimiter.length);
+  const rawContent = source.slice(startDelimiter.length, closeIndex);
+  const content = rawContent.trim();
+  advanceBy(context, closeIndex + closeDelimiter.length);
   return {
     type: NodeTypes.INTERPOLATION,
     content: {
@@ -29,6 +51,9 @@ function parseInterpolation(context) {
       content: content,
     },
   };
+}
+function advanceBy(context, length) {
+  context.source = context.source.slice(length);
 }
 
 function createRoot(children) {
--- src/complier-core/tests/parse.spec.ts
+++ src/complier-core/tests/parse.spec.ts
@@ -3,13 +3,20 @@ import { baseParse } from "../src/parse";
 
 describe("Parse", () => {
   it(" simple interpolation", () => {
-    const ast = baseParse("{{message}}");
+    const ast = baseParse("{{ message }}");
     expect(ast.children[0]).toStrictEqual({
       type: NodeTypes.INTERPOLATION,
       content: {
         type: NodeTypes.SIMPLE_EXPRESSION,
         content: "message",
       },
+    });
+  });
+  it("simple element div", () => {
+    const ast = baseParse("<div></div>");
+    expect(ast.children[0]).toStrictEqual({
+      type: NodeTypes.ELEMENT,
+      tag: "div",
     });
   });
 });
